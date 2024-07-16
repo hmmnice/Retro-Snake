e88f645adf7f22d9ab3d7f6ed5605a1dc736fe8a
ysah <y.bhatia@lancaster.ac.uk>
2024-07-16 08:58:08 +0530

    added ability to save highscore

--- scoreboard.py
+++ scoreboard.py
@@ -4,6 +4,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.penup()
+        
         self.score = 0
         self.color("White")
         self.goto(0,280)
